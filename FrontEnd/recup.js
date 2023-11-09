e5fcfa69724b59896bb7d5819f31f33f8e09c58d
Wilsfried Di Majo <wilsfrieddm@gmail.com>
2023-11-09 21:20:05 +0100

    revamped code for triage dynamique projets (enfin)

--- FrontEnd/recup.js
+++ FrontEnd/recup.js
@@ -1,3 +1,42 @@
+let allData = []; // Store all data fetched from the API
+
+// This function filters the items based on the category name
+function filterSelection(category) {
+    const container = document.querySelector("div.gallery");
+    container.innerHTML = ''; // Clear the container before re-rendering
+
+    if (category === 'all') {
+        allData.forEach(item => {
+            renderGalleryItem(container, item);
+        });
+    } else {
+        allData.forEach(item => {
+            if (item.category.name === category) {
+                renderGalleryItem(container, item);
+            }
+        });
+    }
+}
+
+// Function to render the gallery item
+function renderGalleryItem(container, item) {
+    const figureElement = document.createElement('figure');
+
+    const imageElement = document.createElement('img');
+    imageElement.src = item.imageUrl;
+    imageElement.alt = item.title;
+
+    const figcaptionElement = document.createElement('figcaption');
+    figcaptionElement.innerText = item.title;
+
+    figcaptionElement.classList.add("projectText");
+
+    container.appendChild(figureElement);
+    figureElement.appendChild(imageElement);
+    figureElement.appendChild(figcaptionElement);
+}
+
+// Your existing fetch and rendering logic
 fetch('http://localhost:5678/api/works')
     .then(response => {
         if (response.ok) {
@@ -8,28 +47,14 @@ fetch('http://localhost:5678/api/works')
     })
     .then(data => {
         console.log(data);
+        allData = data; // Store all data fetched from the API
 
-        const container = document.querySelector("div.gallery")
+        const container = document.querySelector("div.gallery");
 
-        data.forEach(item => {
-            const figureElement = document.createElement('figure');
-
-            const imageElement = document.createElement('img');
-            imageElement.src = item.imageUrl;
-            imageElement.alt = item.title;
-
-            const figcaptionElement = document.createElement('figcaption');
-            figcaptionElement.innerText = item.title;
-
-            
-            figcaptionElement.classList.add("projectText")
-
-            container.appendChild(figureElement);
-            figureElement.appendChild(imageElement)
-            figureElement.appendChild(figcaptionElement);
-        });
+        // Call the filter function with the 'all' category to display all items initially
+        filterSelection('all');
 
     })
     .catch(error => {
         console.error('Il y a un problème avec la récupération ( fetch ) des données:', error);
-    })
+    });
